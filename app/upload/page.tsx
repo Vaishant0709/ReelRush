c04c04df0de4768340d9f621d9451dcaeb551be0
Saksham Mittal <sakshammital@gmail.com>
2025-04-08 11:58:57 +0530

    improvement

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -9,13 +9,7 @@ export default function VideoUploadPage() {
         <div className="max-w-2xl mx-auto">
           <div className="mb-8 flex items-center gap-3">
             <div className="bg-gradient-to-br from-purple-500 to-pink-500 p-2 rounded-lg shadow-lg">
-              <svg 
-                xmlns="http://www.w3.org/2000/svg" 
-                viewBox="0 0 24 24" 
-                fill="currentColor" 
-                className="w-6 h-6 text-white">
-                <path d="M9.97 5.97a.75.75 0 011.06 0l3.25 3.25a.75.75 0 010 1.06l-3.25 3.25a.75.75 0 01-1.06-1.06l1.97-1.97H4.75a.75.75 0 010-1.5h7.19L9.97 7.03a.75.75 0 010-1.06z" />
-              </svg>
+             
             </div>
             <h1 className="text-3xl font-bold">
               Upload New <span className="bg-gradient-to-r from-purple-400 to-pink-500 text-transparent bg-clip-text">Reel</span>
